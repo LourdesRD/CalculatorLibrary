e7520f89d887f39e59d3fd8ac02af1b6bdfba117
unknown <lourdes_ruiz_diaz@yahoo.es>
2022-06-03 13:39:12 +0100

    delete blank lines test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -10,10 +10,8 @@ class TestCalculator:
     def test_addition(self):
         assert 4 == calculator.add(2, 2)
 
-
     def test_substraction(self):
         assert 2 == calculator.subtract(4, 2)
-
 
     def test_calculator(self):
         assert 100 == calculator.multiply(10, 10)
